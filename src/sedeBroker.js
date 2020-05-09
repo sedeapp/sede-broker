9c52b1d978e50f35837d4dbf091ca8aeb94cb9a4
Rafael Ribeiro <nokcode86@gmail.com>
2020-05-09 00:22:42 -0300

    Remove semicolons

--- src/sedeBroker.js
+++ src/sedeBroker.js
@@ -8,13 +8,13 @@ class SedeBroker {
             showAnnoyingWarning: true
         }
 
-        this._count = 0;
-        this._sequence = 0;
-        this._subscribers = {};
+        this._count = 0
+        this._sequence = 0
+        this._subscribers = {}
 
         // merge with default options
         for(let key in config) {
-            this.config[key] = config[key];
+            this.config[key] = config[key]
         }
         
         if (this.config.warn && this.config.showAnnoyingWarning) {
@@ -27,53 +27,53 @@ class SedeBroker {
     }
 
     subscribe(proto, handler) {
-        const id = this._sequence++;
-        const broker = this;
+        const id = this._sequence++
+        const broker = this
         const ticket = {
             _proto : proto,
             _handler : handler,
 
             subscribe() {
                 if (broker._subscribers[id]) {
-                    return;
+                    return
                 }
-                broker._subscribers[id] = this;
-                broker._count++;
+                broker._subscribers[id] = this
+                broker._count++
             },
 
             unsubscribe() {
                 if (!broker._subscribers[id]) {
-                    return;
+                    return
                 }
-                delete broker._subscribers[id];
-                broker._count--;
+                delete broker._subscribers[id]
+                broker._count--
             }
         }
         
-        ticket.subscribe();
+        ticket.subscribe()
 
-        return ticket;
+        return ticket
     }
 
     publish(message) {
         // Oh gosh! Really? 
         if (typeof message !== 'object') {
-            throw Error(`The message must be an object!`);
+            throw Error(`The message must be an object!`)
         }
 
         if (this._count == 0 && this.config.warn) {
-            console.warn(`No one subscribed...`);
-            return;
+            console.warn(`No one subscribed...`)
+            return
         }
 
-        const shallow = this._subscribers;
-        const errors = [];
+        const shallow = this._subscribers
+        const errors = []
 
         // keep it simple synchronous
         for (let id in shallow) {
             try {
                 if (message instanceof shallow[id]._proto) {
-                    shallow[id]._handler(message);
+                    shallow[id]._handler(message)
                 }
             }
             catch(e) {
@@ -81,19 +81,19 @@ class SedeBroker {
                     ticket: shallow[id], 
                     message: message, 
                     error: e 
-                });
+                })
             }
         }
 
         // please, promise me you will catch all errors!
         if (errors.length > 0) {
-            throw errors;
+            throw errors
         }
     }
 
     clear() {
-        delete this._subscribers;
+        delete this._subscribers
         this._subscribers = {}
-        this._count = 0;
+        this._count = 0
     }
 }
